19ea874a7e3a3702d3aa197f47d80547ebe61353
Osvaldo TCF <osvaldotcf@gmail.com>
2023-06-24 18:17:58 -0300

    Initial commit

---
+++ interface/src/routes/demo/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from '../$types';
+	import Demo from './Demo.svelte';
+	import MqttConfig from '../connections/MQTT/MQTTConfig.svelte';
+
+	export let data: PageData;
+</script>
+
+<div
+	class="mx-8 my-8 flex
+     flex-col gap-4"
+>
+	<Demo />
+</div>
